a096817c25b2103ab3daad95169f6c188d082c39
macoto00 <macount@seznam.cz>
2023-03-09 12:00:48 +0100

    Fixed bugs in the code 2

--- dal/UserRepository.php
+++ dal/UserRepository.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__."/Repository.php");
-require_once(__DIR__."//../model/User.php");
+require_once(__DIR__."/../model/User.php");
 
 class UserRepository extends Repository 
 {
